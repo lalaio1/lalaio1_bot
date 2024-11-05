2a5dd02e82590a5734d639570cf1f1bcaa0dd9f9
lalaio1 <122741162+lalaio1@users.noreply.github.com>
2024-11-05 19:40:44 -0300

    Delete scripts/site_cloner/www.masterparafusos.com.br directory

--- scripts/site_cloner/www.masterparafusos.com.br/js/custom.js
+++
@@ -1,9 +0,0 @@
-function count_item(item) {  
-    $.ajax({
-        type: "get",
-        url: "count.php",
-        data: "item="+item,
-        success: function (response) {
-        }
-    });
-}
